9613a897b20b82926ccf5b0975b56d5655f2b765
Buffalo <atustotle@gmail.com>
2023-11-30 05:07:47 +0100

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,13 @@
-#!/usr/bin/env python3
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
-
-fizzbuzz()
-print("")
+#!/usr/bin/python3
+def fizzbuzz():
+    '''Print numbers from 1 - 100 in a special way'''
+    for i in range(1, 101):
+        if ((i % 3) == 0) and ((i % 5) == 0):
+            print("FizzBuzz", end=" ")
+        elif (i % 3) == 0:
+            print("Fizz", end=" ")
+        elif (i % 5) == 0:
+            print("Buzz", end=" ")
+        else:
+            print(f'{i}', end=" ")
+    print("")
