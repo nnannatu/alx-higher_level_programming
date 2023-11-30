024f057afb1b74b156952ec4b170c850e7f42063
Buffalo <atustotle@gmail.com>
2023-11-30 02:58:16 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''Program that Print numbers'''
 for number in range(0, 100):
-    print("{:02}".format(number), end=", ")
-print()
+    if number == 99:
+        print('{}'.format(number))
+    else:
+        print("{:02}".format(number), end=", ")
