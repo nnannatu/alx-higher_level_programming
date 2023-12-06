c324c8fa7d2e5791fb085cfb6e178cd835579a3f
Buffalo <atustotle@gmail.com>
2023-12-06 18:12:13 +0100

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-'''Function that computes the square 
-value of all integers of a matrix'''
+'''
+Function that computes the square 
+value of all integers of a matrix
+'''
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
