c6c1f0fe4190c574abdbb1b2637d27a445c0ac21
Buffalo <atustotle@gmail.com>
2023-12-06 19:28:05 +0100

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    '''
+    Function that returns a set of
+    common elements in two sets
+    '''
+    for item in set_1 and set_2:
+        newlist = []
+        newlist.append(item)
+    return newlist
