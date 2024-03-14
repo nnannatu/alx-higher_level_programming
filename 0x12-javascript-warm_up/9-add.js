aa1651439cdbd780297f76063150ba3e72d671d8
Nnanna <atunnannasamson@gmail.com>
2024-03-14 08:38:30 +0100

    delete loop

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,8 +6,4 @@ function add(a, b) {
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-if (!isNaN(num1) && !isNaN(num2)) {
-    console.log(add(num1, num2));
-} else {
-    console.log("Invalid arguments. Please provide two integers.");
-}
+console.log(add(num1, num2));
