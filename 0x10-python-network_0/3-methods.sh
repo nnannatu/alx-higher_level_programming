ad3b6d96207edc27ec9b9a4550fcb618b7b50cee
Nnanna <atunnannasamson@gmail.com>
2024-04-29 14:11:47 +0100

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Extract the URL from the command line argument
-url="$1"
-
-# Display all HTTP methods the server of the given URL will accept
-curl -sI "$url" | awk '/Allow/ {print $2}'
+# Display all HTTP methods the server of a given URL will accept.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
