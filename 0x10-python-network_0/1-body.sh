f4296935c897f7210cdff075ea7d42f32f6796be
Nnanna <atunnannasamson@gmail.com>
2024-04-29 13:52:12 +0100

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided as argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send GET request to the URL and capture the response body
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-# Check if the request was successful (status code 200)
-if [ "$response" -eq 200 ]; then
-    # Display the body of the response
-    curl -s "$1"
-else
-    echo "Request failed with status code: $response"
-fi
+# Get the byte size of the HTTP response header
+curl -s "$1" | wc -c
